cbbdb2d2e9d39323559c31e56997c76427702f97
Rajmund09 <prabhushankarmund@gmail.com>
2025-12-25 00:28:56 +0530

    Add simple and fun JS practice project

---
+++ script.js
@@ -0,0 +1,36 @@
+
+    function generateName() {
+      let rand = Math.random();
+      let first, second, third;
+
+      if (rand < 0.33) {
+        first = "Crazy";
+      } else if (rand < 0.66) {
+        first = "Amazing";
+      } else {
+        first = "Fire";
+      }
+
+      rand = Math.random();
+      if (rand < 0.33) {
+        second = "Engine";
+      } else if (rand < 0.66) {
+        second = "Foods";
+      } else {
+        second = "Garments";
+      }
+
+      rand = Math.random();
+      if (rand < 0.33) {
+        third = "Bros";
+      } else if (rand < 0.66) {
+        third = "Limited";
+      } else {
+        third = "Hub";
+      }
+
+      document.getElementById("outputName").innerText = `${first} ${second} ${third}`;
+    }
+
+    window.onload = generateName;
+  
